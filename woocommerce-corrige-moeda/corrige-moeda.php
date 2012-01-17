00ea3c5c4b8cd45f6b82c5382f1fa849f32e3f6d
hostdesigner <hostdesigner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-17 01:51:04 +0000

    Fix plugin header
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-corrige-moeda/trunk@490895 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-corrige-moeda/corrige-moeda.php
+++ woocommerce-corrige-moeda/corrige-moeda.php
@@ -1,14 +1,14 @@
 <?php 
 /*
-	Plugin Name: WooCommerce Corrige moeda
-	Plugin URI: http://vibemidia.com/woocommerce
-	Description: Corrige a moeda de exibição do WooCommerce de $ 20,00 e para R$ 20,00.
-	Version: 1.0
-	Author: John-Henrique <john@midianegocios.com.br>
-	Author URI: http://vibemidia.com
-	License: GPLv2
-	Requires at least: 3.1
-	Tested up to: 3.3
+Plugin Name: WooCommerce Corrige moeda
+Plugin URI: http://vibemidia.com/woocommerce
+Description: Corrige a moeda de exibição do WooCommerce de $ 20,00 e para R$ 20,00.
+Version: 1.0
+Author: John-Henrique <john@midianegocios.com.br>
+Author URI: http://vibemidia.com
+License: GPLv2
+Requires at least: 3.1
+Tested up to: 3.3
 */
 add_action('plugins_loaded', 'corrige_moeda', 0);
 add_filter('woocommerce_currency_symbol', 'corrige_moeda' );
